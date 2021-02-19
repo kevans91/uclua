5b22057f93bdc48c0016daaa92aa3ef0b2eeb82f
Kyle Evans <kevans@FreeBSD.org>
2021-02-19 07:59:05 -0600

    libuclua: load a subset of the standard libraries
    Notably, load a subset that doesn't inherently include I/O or exec
    operations.  Future work may open these back up under some kind of
    sandbox.

--- libuclua/luclua.c
+++ libuclua/luclua.c
@@ -25,6 +25,8 @@
  * SUCH DAMAGE.
  */
 
+#include <sys/param.h>
+
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -34,12 +36,27 @@
 #include <lauxlib.h>
 
 #include <uclua.h>
+#include "luclua_internal.h"
 
-#define	LCOOKIE_IDX		"luclua_cookie"
+#define	LCOOKIE_IDX		"uclua_cookie"
 
-struct uclua_cookie {
-	lua_State *L;
+static const luaL_Reg dflibs[] = {
+	/* {LUA_LOADLIBNAME, luaopen_package}, */
+	{LUA_COLIBNAME, luaopen_coroutine},
+	{LUA_TABLIBNAME, luaopen_table},
+	/* {LUA_IOLIBNAME, luaopen_io}, */
+	/* {LUA_OSLIBNAME, luaopen_os}, */
+	{LUA_STRLIBNAME, luaopen_string},
+	{LUA_MATHLIBNAME, luaopen_math},
+	{LUA_UTF8LIBNAME, luaopen_utf8},
+	/* {LUA_DBLIBNAME, luaopen_debug}, */
+#if defined(LUA_COMPAT_BITLIB)
+	{LUA_BITLIBNAME, luaopen_bit32},
+#endif
+/*	{"ucl", luaopen_ucl}, */
 };
+
+static void uclua_init_state(lcookie_t *);
 
 lcookie_t *
 uclua_new(void)
@@ -57,7 +74,7 @@ uclua_new(void)
 		goto out;
 
 	lcook->L = L;
-	luaL_openlibs(L);
+	uclua_init_state(lcook);
 
 	*(lcookie_t **)lua_newuserdata(L, sizeof(lcook)) = lcook;
 	lua_setfield(L, LUA_REGISTRYINDEX, LCOOKIE_IDX);
@@ -89,4 +106,26 @@ uclua_free(lcookie_t *lcook)
 
 	lua_close(lcook->L);
 	free(lcook);
+}
+
+static void
+uclua_init_state(lcookie_t *lcook)
+{
+	const luaL_Reg *lib;
+	lua_State *L;
+
+	L = lcook->L;
+	luaL_requiref(L, "_G", luaopen_base, 1);
+	lua_pushnil(L);
+	lua_setfield(L, -2, "dofile");
+
+	lua_pushnil(L);
+	lua_setfield(L, -2, "loadfile");
+	lua_pop(L, 1);
+
+	for (size_t i = 0; i < nitems(dflibs); ++i) {
+		lib = &dflibs[i];
+		luaL_requiref(L, lib->name, lib->func, 1);
+		lua_pop(L, 1);
+	}
 }
